d4467c3cddd3ca4d1ddc37c81f8583f9a0a7f3f3
Yevhen Beshkarov <ybeshkarov@saymyframe.com>
2025-07-15 11:08:44 +0300

    SMF Contribution Engine - allows contributing code into existing files.

---
+++ packages/smf_contribution_engine/lib/src/contributions/contributions.dart
@@ -0,0 +1,5 @@
+export 'insert_import.dart';
+export 'insert_into_function.dart';
+export 'insert_into_list_in_function.dart';
+export 'insert_into_list_in_method_in_class.dart';
+export 'insert_into_method_in_class.dart';
