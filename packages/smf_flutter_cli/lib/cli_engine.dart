8c5a6b76cede8ff425fae7f3ed4fae7bd571fbf6
Yevhen Beshkarov <ybeshkarov@saymyframe.com>
2025-07-15 13:02:34 +0300

    SMF CLI engine shared contribution changes.

--- packages/smf_flutter_cli/lib/cli_engine.dart
+++ packages/smf_flutter_cli/lib/cli_engine.dart
@@ -1,12 +1,10 @@
 import 'dart:io';
 
 import 'package:mason/mason.dart';
-import 'package:mustachex/mustachex.dart';
 import 'package:smf_analytics/smf_analytics.dart';
 import 'package:smf_contracts/smf_contracts.dart';
 import 'package:smf_flutter_cli/bundles/smf_cli_brick_bundle.dart';
 import 'package:smf_flutter_cli/utils/module_dependency_resolver.dart';
-import 'package:smf_flutter_cli/utils/utils.dart';
 import 'package:smf_flutter_core/smf_flutter_core.dart';
 import 'package:yaml_edit/yaml_edit.dart';
 
@@ -84,79 +82,19 @@ Future<void> _generateBrickContributions(
 }
 
 Future<void> _generateSharable(
-  List<IModuleCodeContributor> selectedModules,
+  List<IModuleCodeContributor> modules,
   Logger logger,
   Map<String, dynamic> coreVars,
 ) async {
-  // Group shared contributions by bundle and slot
-  final sharedContributions =
-      <String, Map<String, List<SharedFileContribution>>>{};
+  final contributions =
+      modules.map((m) => m.sharedFileContributions).expand((e) => e).toList();
 
-  for (final module in selectedModules) {
-    for (final contribution in module.sharedFileContributions) {
-      final bundleKey = contribution.bundle.name;
-      sharedContributions.putIfAbsent(bundleKey, () => {});
-      sharedContributions[bundleKey]!
-          .putIfAbsent(contribution.slot.tag, () => []);
-      sharedContributions[bundleKey]![contribution.slot.tag]!.add(contribution);
-    }
-  }
-
-  // Process each bundle's shared contributions
-  for (final bundleKey in sharedContributions.keys) {
-    final slotContributions = sharedContributions[bundleKey]!;
-
-    // Find the bundle from any contribution
-    final sampleContribution = slotContributions.values.first.first;
-    final bundle = sampleContribution.bundle;
-
-    final generator = await MasonGenerator.fromBundle(bundle);
-    final target = DirectoryGeneratorTarget(Directory(testPath));
-
-    // Prepare variables for the bundle
-    final vars = coreVars..addAll(sampleContribution.vars ?? {});
-
-    // Processor for shared contribution content
-    final processor = MustachexProcessor(initialVariables: vars);
-
-    // Process each slot
-    for (final slot in slotContributions.keys) {
-      final contributions = slotContributions[slot]!
-
-        // Sort by order if specified
-        ..sort((a, b) =>
-            (a.order ?? maxIntValue).compareTo(b.order ?? maxIntValue));
-
-      final renderedContents = await Future.wait(
-        contributions.map((c) => processor.process(c.content)),
-      );
-      final combinedContent = renderedContents.join();
-      vars[slot] = combinedContent;
-    }
-
-    await generator.hooks.preGen(
-      vars: vars,
-      onVarsChanged: vars.addAll,
-      logger: logger,
-    );
-
-    final generateProgress = logger.progress(
-      '🔄 Generating shared content for $bundleKey',
-    );
-
-    final files = await generator.generate(
-      target,
-      vars: vars,
-      fileConflictResolution: FileConflictResolution.overwrite,
-      logger: logger,
-    );
-
-    generateProgress.complete(
-      '✅ Generated shared content in ${files.length} file(s)',
-    );
-
-    await generator.hooks.postGen(vars: vars, logger: logger);
-  }
+  await PatchEngine(
+    contributions,
+    projectRoot: coreVars[kWorkingDirectory] as String,
+    mustacheVariables: coreVars,
+    logger: logger,
+  ).applyAll();
 }
 
 Future<void> _generatePubspecDependencies(
