056a932d65930cdefed01e40a9b44122b5aa55c2
Yevhen Beshkarov <ybeshkarov@saymyframe.com>
2025-08-09 16:24:54 +0300

    smf contracts moved to the separate repository

---
+++ packages/smf_contracts/lib/smf_contracts.dart
@@ -0,0 +1,14 @@
+library;
+
+export 'smf_contracts_module.dart';
+export 'src/brick_contribution.dart';
+export 'src/constants/constants.dart';
+export 'src/di/di_contract.dart';
+export 'src/file_merge_strategy.dart';
+export 'src/generators/aware_code_generator.dart';
+export 'src/generators/generators.dart';
+export 'src/import/import_barrel.dart';
+export 'src/module_contributor.dart';
+export 'src/module_descriptor.dart';
+export 'src/mustache/mustache.dart';
+export 'src/routing/routing.dart';
