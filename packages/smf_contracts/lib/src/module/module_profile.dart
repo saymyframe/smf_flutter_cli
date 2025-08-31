87ff4f68185acf0bb3436c292b0f43f354a78a34
Yevhen Beshkarov <ybeshkarov@saymyframe.com>
2025-08-31 17:09:02 +0300

    feat: add StateManager enum for bloc and riverpod support.

--- packages/smf_contracts/lib/src/module/module_profile.dart
+++ packages/smf_contracts/lib/src/module/module_profile.dart
@@ -1,4 +1,11 @@
-enum StateManager { bloc, riverpod }
+enum StateManager {
+  bloc._('bloc'),
+  riverpod._('riverpod');
+
+  const StateManager._(this.stateManager);
+
+  final String stateManager;
+}
 
 class ModuleProfile {
   const ModuleProfile({required this.stateManager});
